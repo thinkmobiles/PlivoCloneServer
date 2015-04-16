8a0acf285d7e409f0c96a770c3e4ce9336e06629
mmmigalll <vashkebam1991@gmail.com>
2015-04-16 12:56:04 +0300

    fileStorage

---
+++ modules/fileStorage.js
@@ -0,0 +1,50 @@
+/**
+ * Created by User on 16.04.2015.
+ */
+var fs = require('fs');
+var path = require('path');
+
+module.exports = function(){
+    'use strict'
+    var fileStorage = function () {
+        var self = this;
+
+        this.postFile = function (folderName, fileName, options, callback) {
+            var targetPath = folderName;
+            var filePath = path.join(folderName, fileName);
+            if (fs.existsSync(targetPath)) {
+                writeFile(filePath, options.data, callback);
+            } else {
+                makeDir(targetPath, function (err) {
+                    if (err) {
+                        if (callback) {
+                            callback(err);
+                        } else {
+                            console.error('Make dir error ' + err.message);
+                        }
+                    } else {
+                        writeFile(filePath, options.data, callback);
+                    }
+                });
+            }
+        };
+
+        function writeFile(filePath, fileData, callback) {
+            try {
+                fs.writeFile(filePath, fileData, function (err, data) {
+                    if (callback && typeof callback === 'function') {
+                        callback(err)
+                    }
+                });
+            }
+            catch (err) {
+                console.log('ERROR:', err);
+                if (callback && typeof callback === 'function') {
+                    callback(err)
+                }
+            }
+        }
+
+    };
+    return fileStorage;
+}
