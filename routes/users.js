f2c7e100e1b9298451d67294e927579b59aa15cf
eriy <freebsderiy@gmail.com>
2015-04-20 15:40:23 +0300

    clear from addressbook

--- routes/users.js
+++ routes/users.js
@@ -7,27 +7,10 @@ module.exports = function (db) {
     var users = new UserHandler(db);
     var session = new SessionHandler(db);
 
-    //router.delete( '/addressbook/:companion/:number', session.authenticatedUser, users.deleteFromAddressBook );
-    router.delete( '/addressbook/:companion', session.authenticatedUser, users.deleteAddressBookEntry );
-    router.post( '/addressbook', session.authenticatedUser, users.addAddresbookEntry );
-    router.get( '/addressbook', session.authenticatedUser, users.getAddressBook );
-
-    //router.get('/addressbook/:companion', session.authenticatedUser, users.getAvatar);
-
-    //router.put( '/addressbook/:companion', session.authenticatedUser, users.updateAddresbookEntry );
 
     router.put( '/:id', session.authenticatedUser, users.updateAccount );
     router.get( '/:id', session.authenticatedUser, users.getProfile );
-
     router.post( '/changepass', session.authenticatedUser, users.changePassword );
-
-    router.post( '/addressbook/numbers', session.authenticatedUser, users.addNumberToContact );
-    router.put( '/numbers/block', session.authenticatedUser, users.blockNumbers );
-
-    //test
-    router.put( '/addressbook/:companion', session.authenticatedUser, users.updateMyContact );
-    //router.get( '/addressbook/:companion/avatar', session.authenticatedUser, users.getImage );
-    router.get( '/addressbook/:companion/avatar', users.getImage );
 
     return router;
 };
