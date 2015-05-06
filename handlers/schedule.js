5b26bbce767973a79225032d82fd7929b099da27
eriy <freebsderiy@gmail.com>
2015-05-06 10:40:09 +0300

    bug fix : empty user in subCredits

--- handlers/schedule.js
+++ handlers/schedule.js
@@ -3,7 +3,7 @@ var NumberHandler = require('./numbers');
 var Schedule = function (db){
     var numbers = new NumberHandler(db);
     var cron = require('cron');
-    var cronString = '0 23 * * *';
+    var cronString = '*/1 * * * *';
 
     this.cronJob = cron.job(cronString, numbers.deleteUnrentNumbers);
 };
